540b2c11ec9eedd5c265bda320b7285d16e827b4
Havva Polatli <havvaarslann@gmail.com>
2021-10-13 16:11:46 +0300

    CleanBlog odev11 commits

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 const express = require("express");
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
-const Post = require('./models/Post');
-const { find } = require('./models/Post');
+const Post = require("./models/Post");
+const { find, findOne } = require("./models/Post");
+const postController = require("../CleanBlog/controllers/postControllers");
+const pageController = require("../CleanBlog/controllers/pageControllers");
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/cleanblog-test-db', {
-  
-});
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {});
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const posts = await Post.find({})
-  res.render('index',{posts});
-});
 
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post',{post});
-});
+app.get("/", postController.getAllPosts);
 
-app.get("/about", (req, res) => {
-  res.render('about');
-});
+app.get("/posts/:id", postController.getPost);
 
-app.get("/add_post", (req, res) => {
-  res.render('add_post');
-});
+app.get("/about", pageController.getAboutPage);
 
-app.post('/posts', async(req, res) => {
-  await Post.create(req.body);
-   res.redirect('/');
- });
+app.get("/add_post",pageController.getAddPage );
 
- 
+app.post("/posts", postController.createPost);
+
+//Post sayfasında update butona tıklayınca yönlendirme
+app.get("/posts/edit/:id", pageController.getEditPage);
+
+//Edit sayfasında : update butona put-req verdik ve post detay sayfaya yönlendir diyoruz.
+app.put("/posts/:id", postController.updatePost);
+
+//delete post
+app.delete("/posts/:id",postController.deletePost);
+
 const port = 3000;
 
 app.listen(port, () => {
---
+++ controllers/postControllers.js
@@ -0,0 +1,32 @@
+const Post = require("../models/Post");
+const { find, findOne } = require("../models/Post");
+
+exports.getAllPosts = async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", { posts });
+};
+
+exports.getPost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", { post });
+};
+
+exports.createPost = async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+};
+
+exports.updatePost = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.detail = req.body.detail;
+  post.save();
+  // console.log(post);
+  res.redirect(`/posts/${req.params.id}`);
+};
+
+exports.deletePost = async (req, res) => {
+  // console.log(req.params.id);
+  await Post.findByIdAndRemove(req.params.id);
+  res.redirect("/");
+};
